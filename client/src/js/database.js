58828653c6d530385e83322e1a699f8f93ea7c2d
Coffee Pawz <lauralynneduffy@gmail.com>
2022-12-23 12:10:45 -0500

    wrote logic for the TODOs in /src/database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const jateDb = await openDB('JATE', 1);
+    const transactionJate = jateDb.transaction('JATE', 'readwrite');
+    const jateObjectStore = transactionJate.objectStore('JATE');
+    const putRequest = jateObjectStore.put({id: id, text: content});
+    const result = await putRequest; 
+    console.log('saved', result);
+  } catch {
+    console.error('not saved. putDb not implemented');
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const jateDb = await openDB('JATE', 1); 
+    const transactionJate = jateDb.transaction('JATE', 'readonly'); 
+    const jateObjectStore = transactionJate.objectStore('JATE');
+    const getAllRequest = jateObjectStore.getAll(); 
+    const result = await getAllRequest; 
+    return result;
+  } catch {
+    console.error('error with getAll request. getDb not implemented');
+  }
+}
 
 initdb();
